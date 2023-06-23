a218848d684093725251477dd5d03899e627002e
peiyuwu <ts01174755@gmail.com>
2023-06-23 22:57:12 +0800

    first commit

--- automation/build_all_environment.py
+++ automation/build_all_environment.py
@@ -5,8 +5,8 @@ from  src.controller.build_environment import BuildEnvironment as BuildEnvironme
 if __name__ == "__main__":
     print("Building all environments")
 
-    # BuildEnvironment.build_local_environment(RUN='all', config=env_config)
-    # BuildEnvironment.build_monodb_environment(RUN='all', config=env_config)
-    # BuildEnvironment.build_postgres_environment(RUN='all', config=env_config)
+    BuildEnvironment.build_local_environment(RUN='all', config=env_config)
+    BuildEnvironment.build_monodb_environment(RUN='all', config=env_config)
+    BuildEnvironment.build_postgres_environment(RUN='all', config=env_config)
 
     subprocess.run("pip3 freeze > requirment.txt", shell=True)
